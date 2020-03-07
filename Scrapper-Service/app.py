7d6162e9473f64c8323560cfe7796feed8547c4b
Sanchit Khurana <sanchitkhuranask@gmail.com>
2020-03-07 21:45:41 +0530

    fix(app.py): Spell fix for couple of variables

--- Scrapper-Service/app.py
+++ Scrapper-Service/app.py
@@ -4,71 +4,108 @@ import argparse
 import importlib
 import traceback
 import os
-from utility import str2bool , getLogger , printStart
-
-
+from utility import str2bool, getLogger, printStart
 
 
 ##
 #
 # Test whether the initialization is working or not
 # >> python app.py -l debug -ls console  --test True
-# 
+#
 # Test whether the run is working or not
 # >> python app.py -l debug -ls file --test False
-#  
-# 
+#
+#
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c" , "--config",  default='config.json' , type=str , action='store'
-                                     ,  dest="config" , help="Specify config.json file")
-parser.add_argument("-l" , "--log" ,    default="debug"  , action='store', dest="log_level" ,
-                                        choices=["debug" , "warn" , "error" , "info"]  , help="Specify the debug level ,default: %(default)s")
-parser.add_argument("-t" , "--test" ,   default=True , type=str2bool , action="store" , dest="test" , 
-                                        help="Specify whether to test app initialization or run the scrappers ,default: %(default)s")
-parser.add_argument("-ls", "--logStream", default="console", type=str , action="store" , dest="log_stream" , 
-                                          choices=["console" , "file"],
-                                          help="Specify whether to print logs on terminal or to file ,default: %(default)s"  )
+parser.add_argument(
+    "-c",
+    "--config",
+    default="config.json",
+    type=str,
+    action="store",
+    dest="config",
+    help="Specify config.json file",
+)
+parser.add_argument(
+    "-l",
+    "--log",
+    default="debug",
+    action="store",
+    dest="log_level",
+    choices=["debug", "warn", "error", "info"],
+    help="Specify the debug level ,default: %(default)s",
+)
+parser.add_argument(
+    "-t",
+    "--test",
+    default=True,
+    type=str2bool,
+    action="store",
+    dest="test",
+    help="Specify whether to test app initialization or run the scrappers ,default: %(default)s",
+)
+parser.add_argument(
+    "-ls",
+    "--logStream",
+    default="console",
+    type=str,
+    action="store",
+    dest="log_stream",
+    choices=["console", "file"],
+    help="Specify whether to print logs on terminal or to file ,default: %(default)s",
+)
 values = parser.parse_args()
 
-LOG_LEVEL_DEFAULTS = {"debug":logging.DEBUG , "warn":logging.WARN , "info":logging.INFO , "error":logging.ERROR}
-LOG_STREAM_DEFAULTS = { "file":lambda filename:logging.FileHandler(filename),
-                        "console":lambda filname: logging.StreamHandler()
-                        }
+LOG_LEVEL_DEFAULTS = {
+    "debug": logging.DEBUG,
+    "warn": logging.WARN,
+    "info": logging.INFO,
+    "error": logging.ERROR,
+}
+LOG_STREAM_DEFAULTS = {
+    "file": lambda filename: logging.FileHandler(filename),
+    "console": lambda filename: logging.StreamHandler(),
+}
 
 if values.log_level.lower() not in LOG_LEVEL_DEFAULTS:
-    raise ValueError("Unsupported log level. Supported levels: debug , warn , info , error")
+    raise ValueError(
+        "Unsupported log level. Supported levels: debug , warn , info , error"
+    )
 if values.log_stream.lower() not in LOG_STREAM_DEFAULTS:
     raise ValueError("Unsupported log stream. Supported levels: file , console")
 
 log_level = LOG_LEVEL_DEFAULTS[values.log_level.lower()]
 
-istest = values.test
+is_test = values.test
 
 CONFIG = values.config
 
 log_streamOption = LOG_STREAM_DEFAULTS[values.log_stream.lower()]
- 
+
 
 def createDatabase(configuration):
     db_configuration = configuration["database"]
     path = db_configuration["plugin"]["filename"]
     classname = db_configuration["plugin"]["class"]
-    module = importlib.import_module(path , ".")
+    module = importlib.import_module(path, ".")
     Database = module.__getattribute__(classname)
-    return lambda logger: Database(logger , **db_configuration) 
+    return lambda logger: Database(logger, **db_configuration)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     with open(CONFIG) as file:
         configuration = json.load(file)
-    
+
     logging_configuration = configuration["logging"]
     log_folder = logging_configuration["output"]
     if not log_folder in os.listdir():
         os.mkdir(log_folder)
-    
-    logger = getLogger(__name__ , log_level ,  log_streamOption("{}/{}.log".format(log_folder , "main")) )
+
+    logger = getLogger(
+        __name__, log_level, log_streamOption("{}/{}.log".format(log_folder, "main"))
+    )
 
     printStart(logger)
     logger.info("Application started , Extracting all the plugins")
@@ -78,14 +115,22 @@ if __name__ == '__main__':
 
         path = attr["filename"]
         class_name = attr["class"]
-        plugin_module = importlib.import_module(path , ".")
+        plugin_module = importlib.import_module(path, ".")
         scrapper = plugin_module.__getattribute__(class_name)
         try:
-            log_stream = log_streamOption("{}/{}.log".format(log_folder , class_name))
-            if istest:
-                scrapper( log_level = log_level, log_stream = log_stream , getDatabaseObject = createDatabase(configuration) )
+            log_stream = log_streamOption("{}/{}.log".format(log_folder, class_name))
+            if is_test:
+                scrapper(
+                    log_level=log_level,
+                    log_stream=log_stream,
+                    getDatabaseObject=createDatabase(configuration),
+                )
             else:
-                scrapper( log_level = log_level, log_stream = log_stream , getDatabaseObject = createDatabase(configuration) ).run()
+                scrapper(
+                    log_level=log_level,
+                    log_stream=log_stream,
+                    getDatabaseObject=createDatabase(configuration),
+                ).run()
         except Exception as e:
             logger.error("{} scrapper failed".format(class_name))
             traceback.print_exception(type(e), e, e.__traceback__)
