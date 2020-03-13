867cbeb9f690594c36830759e5b79ff9e0403ccd
abhishekgupta368 <abhigupta9087@gmail.com>
2020-03-13 20:32:24 +0530

    update_MultiprocessContext

--- Scrapper-Service/process/mutiprocessing.py
+++ Scrapper-Service/process/mutiprocessing.py
@@ -4,22 +4,39 @@ from multiprocessing import Process
 ## 1. Add multiprocessing from the reference implementation
 ## 2. Use Logging at properplaces for generating logs from the context manager
 class MultiProcessingContext:
+    
     def __init__(self , log_level , log_stream):
         """[Multi Processing class]
             Responsible for running the lambda functions passed in
             inside threads
+            Arguments:
+                log_level {[string]} -- Levels of log for each process
+                log_stream {[string]} -- Stream of log for each process
+                
         """
         self.logger = get_logger(__name__ , log_level , log_stream)
         self.process_list=[]
     def __execute__(self , runnable):
+        """Start execution of MultiProcessingContext 
+            Returns:
+                None
+        """
+        self.logger.info('''
+                         Thread Process inited
+                         initiated
+                         ''')
+        ## start process for each call
         p= Process(target= runnable)
+        ## append the each process in list
         self.process_list.append(p)
+        ## start calling process
         p.start()
 
     def __enter__(self):
         return self.__execute__ 
 
     def __exit__(self , exception_type, exception_value, traceback):
+        ## job of each process is completed
         for process in self.process_list:
             process.join()
 
