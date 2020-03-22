c86e13b18e36748e1c8d56d4d56dced6f85f48f9
abhishekgupta368 <abhigupta9087@gmail.com>
2020-03-22 17:34:09 +0530

    recover_server_ts

---
+++ Search-Service/src/server.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+/* 
+Load the .env file into the process environment variable scope
+It's necessary to keep a .env file in the project root
+along side package.json
+*/
+dotenv.config() 
