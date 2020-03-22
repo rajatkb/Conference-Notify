43860e63bdfb26bac99f8a5ff6e809ce0cfb0666
Abhishek Gupta <39022530+abhishekgupta368@users.noreply.github.com>
2020-03-22 16:12:01 +0530

    delete server_ts

--- Search-Service/src/server.ts
+++
@@ -1,8 +0,0 @@
-console.log("Test run")
-import * as dotenv from 'dotenv';
-/* 
-Load the .env file into the process environment variable scope
-It's necessary to keep a .env file in the project root
-along side package.json
-*/
-dotenv.config()
