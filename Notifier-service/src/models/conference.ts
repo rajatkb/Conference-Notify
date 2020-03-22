de0e4a3cd3927fc12f33814023fc094eb34a1abb
kajol <kajolkumarisingh222@gmail.com>
2020-03-22 14:15:43 +0530

    all_missing_model_functions_added

--- Notifier-service/src/models/conference.ts
+++ Notifier-service/src/models/conference.ts
@@ -1,65 +1,65 @@
 import mongoose from 'mongoose';
 import { Connection } from 'mongoose';
-import { Conference , ConferenceDocument , ConferenceSchema } from '../schemas/conferences';
+import { Conference, ConferenceDocument, ConferenceSchema } from '../schemas/conferences';
 import { Database } from '../interfaces/database';
 import { ConferenceModel } from '../interfaces/models/conference';
 import { Logger } from '../utility/log';
 
 
 
-export class ConferenceModelI extends ConferenceModel{
+export class ConferenceModelI extends ConferenceModel {
     modelName = "conference"
-    private model:Promise<mongoose.Model<ConferenceDocument,{}>>;
-    private connection:Promise<Connection>;
+    private model: Promise<mongoose.Model<ConferenceDocument, {}>>;
+    private connection: Promise<Connection>;
     private logger = new Logger(this.constructor.name).getLogger();
-    constructor(database:Database){
+    constructor(database: Database) {
         super(database)
-        
+
         // this.connection = connection
         // this.model = this.connection.model<ConferenceDocument>( this.modelName, ConferenceSchema);
 
         this.connection = database.getConnection()
-        .then( (connection:Connection) => {
-            return Promise.resolve(connection);
-        } )
-        .catch((error) => {
-            let errstring = "Failed at getting connection :"+error;
-            this.logger.error(errstring);
-            return Promise.reject(error);
-        })
+            .then((connection: Connection) => {
+                return Promise.resolve(connection);
+            })
+            .catch((error) => {
+                let errstring = "Failed at getting connection :" + error;
+                this.logger.error(errstring);
+                return Promise.reject(error);
+            })
 
         this.model = this.connection
-        .then( (connection: Connection) =>{
-            let model = connection.model<ConferenceDocument>( this.modelName, ConferenceSchema);
-            return Promise.resolve(model);
-        } )
-        .catch((error) => {
-            let errstring = "Failed at getting connection for model"+error;
-            this.logger.error(errstring);
-            return Promise.reject(error);
-        })
+            .then((connection: Connection) => {
+                let model = connection.model<ConferenceDocument>(this.modelName, ConferenceSchema);
+                return Promise.resolve(model);
+            })
+            .catch((error) => {
+                let errstring = "Failed at getting connection for model" + error;
+                this.logger.error(errstring);
+                return Promise.reject(error);
+            })
 
     }
-    
-    private async  makeQuery<T>(callback:(model:mongoose.Model<ConferenceDocument,{}>) => Promise<T>):Promise<T> {
+
+    private async  makeQuery<T>(callback: (model: mongoose.Model<ConferenceDocument, {}>) => Promise<T>): Promise<T> {
         return this.model
-        .then(callback)
-        .catch(error => {
-            this.logger.debug("Failed at getOne: model not initialised error:"+error);
-            this.logger.error("Failed at getOne : model must have failed to initialize :"+error);
-            return Promise.reject(new Error("model failed to be initialised"));
-        });                
+            .then(callback)
+            .catch(error => {
+                this.logger.debug("Failed at getOne: model not initialised error:" + error);
+                this.logger.error("Failed at getOne : model must have failed to initialize :" + error);
+                return Promise.reject(new Error("model failed to be initialised"));
+            });
     }
 
 
-    async getOne():Promise<ConferenceDocument | null> {
+    async getOne(): Promise<ConferenceDocument | null> {
         let result = this.makeQuery((model) => {
-            return new Promise<ConferenceDocument | null>( (resolve , reject) => {
-                model.findOne({} , (err , res) => {
-                    if(!err){
+            return new Promise<ConferenceDocument | null>((resolve, reject) => {
+                model.findOne({}, (err, res) => {
+                    if (!err) {
                         resolve(res);
                     }
-                    else{
+                    else {
                         reject(err);
                     }
                 })
@@ -71,33 +71,55 @@ export class ConferenceModelI extends ConferenceModel{
     /*
         TO-DO
     */
-    async getConferences(offset:number , range:number):Promise<ConferenceDocument[] | null> {
-        let result = this.makeQuery((model) => {
-            return new Promise<ConferenceDocument[] | null>( (resolve , reject) => {
-                model.find({}).limit(range).exec((err , res) => {
-                    if(!err){
+    async getConferences(offset: number, range: number): Promise<ConferenceDocument[] | null> {
+
+        let result = this.model.then(model => {
+            return new Promise<ConferenceDocument[] | null>((resolve, reject) => {
+                model.find({ "deadline": { $gte: new Date() } }, (err, res) => {
+                    if (!err) {
                         resolve(res);
                     }
-                    else{
+                    else {
                         reject(err);
                     }
-                })
+                }).sort({ 'deadline': 1 }).skip(offset).limit(range);
             })
         })
-
-        return result;
+        return result
     }
 
-    /*
-        TO-DO
-    */
-    async getConferencesFromCategory(category:string , offset:number , range:number):Promise<ConferenceDocument[] | null> {
-        return Promise.resolve(null);
+    async getConferencesFromCategory(category: string, offset: number, range: number): Promise<ConferenceDocument[] | null> {
+        let result = this.model
+            .then(model => {
+                return new Promise<ConferenceDocument[] | null>((resolve, reject) => {
+                    model.find({ 'categories': { $in: [category] } }, (err, res) => {
+                        if (!err) {
+                            resolve(res);
+                        }
+                        else {
+                            reject(err);
+                        }
+                    }).skip(offset).limit(range);
+                })
+            })
+        return result
     }
 
-
-    async getCategories():Promise<Array<string> | null> {
-        return Promise.resolve(null);
+    async getCategories(): Promise<any> {
+        let result = this.model
+            .then(model => {
+                return new Promise<ConferenceDocument[] | null>((resolve, reject) => {
+                    model.distinct(('categories'), (err, res) => {
+                        if (!err) {
+                            resolve(res);
+                        }
+                        else {
+                            reject(err);
+                        }
+                    })
+                })
+            })
+        return result
     }
 
     /*
