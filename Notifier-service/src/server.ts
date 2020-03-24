2fc78ae0d4f7a0a58e0e8ec17553a396bd8bde5e
kajol <kajolkumarisingh222@gmail.com>
2020-03-24 12:23:41 +0530

    removed NaN check

--- Notifier-service/src/server.ts
+++ Notifier-service/src/server.ts
@@ -18,14 +18,9 @@ dotenv.config({
 */
 
 
-// Ensure required SERVER_PORT PORT have datatype of number
-if(isNaN(Number(process.env.SERVER_PORT))) {
-    throw new Error('The SERVER_PORT must be a number!')
-} else {
-        let app = new App();
-        app.init()
-        app.start((port) => {
-        console.log("Listening on port :"+port);
-    })
-}
+let app = new App();
+app.init()
+app.start((port) => {
+console.log("Listening on port :"+port);
+})
 
