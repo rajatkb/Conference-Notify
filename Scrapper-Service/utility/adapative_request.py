a9d564511000121a091bfe39e829045bed6dc1f2
manasakalaga <sreemanasa.2000@gmail.com>
2020-03-26 15:36:45 +0530

    [scrapper utility] add redirect functionality to adaptive_request

--- Scrapper-Service/utility/adapative_request.py
+++ Scrapper-Service/utility/adapative_request.py
@@ -6,9 +6,13 @@ class AdaptiveRequest:
         self.max_wait_time = 10
         self.num_fail = 0
         self.num_success = 0
-    def get(self , link ):
+    def get(self , link, **kwargs ):
         try:
-            res = requests.get(link , timeout = self.max_wait_time)
+            redirects = kwargs.get('redirects')
+            if redirects and (redirects == True):
+                res = requests.get(link , timeout = self.max_wait_time, allow_redirects=True)
+            else:
+                res = requests.get(link , timeout = self.max_wait_time)
             self.num_success +=1
             return res
         except (requests.HTTPError , requests.ConnectionError) as err:
