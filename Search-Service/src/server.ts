ffa7d2e06e17fb964007f6a7640b15d6b0b960b8
Abhishek Gupta <39022530+abhishekgupta368@users.noreply.github.com>
2020-03-27 13:37:08 +0530

    correct server_file

--- Search-Service/src/server.ts
+++ Search-Service/src/server.ts
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import * as dotenv from 'dotenv-safe';
-=======
-import * as dotenv from 'dotenv';
->>>>>>> 0803630106183622cfcbcc3d332c4214d6a68fc7
 /* 
 Load the .env file into the process environment variable scope
 It's necessary to keep a .env file in the project root
 along side package.json
 */
-<<<<<<< HEAD
-dotenv.config() 
-=======
 dotenv.config({
     example: './.env'
 });
-=======
-dotenv.config()
->>>>>>> 0803630106183622cfcbcc3d332c4214d6a68fc7
