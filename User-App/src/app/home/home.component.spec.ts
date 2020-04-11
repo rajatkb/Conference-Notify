b23e89bf607254117fc491ca7279a1541f8a0d88
kajol <kajolkumarisingh222@gmail.com>
2020-04-11 15:47:32 +0530

    modified_User-App_For_passing_build

--- User-App/src/app/home/home.component.spec.ts
+++ User-App/src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, RouterModule ]
     })
     .compileComponents();
   }));
