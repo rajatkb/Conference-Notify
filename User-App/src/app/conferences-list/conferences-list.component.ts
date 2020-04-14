a0b812e64fc047c7ab0c5f623461894743134fdb
kajol <kajolkumarisingh222@gmail.com>
2020-04-14 21:26:12 +0530

    fixed_display_conference_list_based_on_scroll_event

--- User-App/src/app/conferences-list/conferences-list.component.ts
+++ User-App/src/app/conferences-list/conferences-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Conference } from '../models/conference.model';
 import { ConferencesService } from '../services/conferences.service';
 
@@ -9,26 +8,32 @@ import { ConferencesService } from '../services/conferences.service';
   styleUrls: ['./conferences-list.component.css']
 })
 
-
 export class ConferencesListComponent implements OnInit, OnDestroy {
-public conferences: Conference[];
+public conferences: Conference[] = [];
+public conferenceList: Array<Conference[]> = [];
 offset = 0;
 count = 3;
 constructor( public conferencesservice: ConferencesService) { }
 
 @HostListener('window:scroll', ['$event'])
     scrollHandler(event) {
-      console.log('Scroll Event');
-      console.log(this.offset, this.count);
       this.getConferenceslist(this.offset, this.count);
     }
 
-ngOnInit() {
-  this.getConferenceslist(this.offset, this.count);
+  ngOnInit() {
+    this.getConferenceslist(this.offset, this.count);
   }
 
   getConferenceslist(offSet, Count) {
-    this.conferences = this.conferencesservice.getConferences(offSet, Count);
+    this.conferenceList.push(this.conferencesservice.getConferences(offSet, Count));
+    const len = this.conferenceList.length;
+    let i = 0;
+    while (i < len) {
+      this.conferenceList[i].forEach(conference => {
+        this.conferences.push(conference);
+      });
+      i = i + 1;
+    }
     this.offset += this.count;
     this.count += this.count;
   }
